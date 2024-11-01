3bc2651a5d204394a730efa1545f60690b67ad09
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-11-01 09:29:23 -0400

    Update file_2.py

--- python-files-SteppCB-main/python-files-SteppCB-main/file_2.py
+++ python-files-SteppCB-main/python-files-SteppCB-main/file_2.py
@@ -2,16 +2,16 @@ import os
 
 def count_lines_words_in_file(file_path):
     try:
-        # Check if the file exists
-        if exists:
-            # Open the file in read mode
-                content = ""
-                lines = ""
-                words = ""
-                print(f"Number of lines: {len(lines)}")
+        # Checking if the file exists
+        if os.path.exists(file_path):
+            # Opening the file in read mode
+            with open(file_path, 'r') as file:
+                content = file.readlines()
+                words = [word for line in content for word in line.split()]
+                print(f"Number of lines: {len(content)}")
                 print(f"Number of words: {len(words)}")
         else:
+            # If the file does not exist, print an error message
             print(f"The file '{file_path}' does not exist.")
-
     except Exception as e:
         print(f"An error occurred: {e}")
