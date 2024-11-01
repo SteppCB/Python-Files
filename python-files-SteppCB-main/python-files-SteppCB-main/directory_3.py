a4507d808ba5b447fd6e31d7b74f42c1c53aee13
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-11-01 09:28:37 -0400

    Update directory_3.py

--- python-files-SteppCB-main/python-files-SteppCB-main/directory_3.py
+++ python-files-SteppCB-main/python-files-SteppCB-main/directory_3.py
@@ -3,19 +3,21 @@ import shutil
 
 def create_directory_and_copy_files(source_dir, destination_dir):
     try:
-        # Create the destination directory if it doesn't exist
+        # Creating the destination directory if it doesn't exist
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
 
+        # Files in the source directory
+        files_to_copy = os.listdir(source_dir)
 
-
-        # List files in the source directory
-        files_to_copy = ""
-
+        # Copying each file 
         for file in files_to_copy:
-            source_file = ""
-            destination_file = ""
-            # copy the file
+            source_file = os.path.join(source_dir, file)
+            destination_file = os.path.join(destination_dir, file)
+            # Restrictions to only copy files, not directories
+            if os.path.isfile(source_file):
+                shutil.copy(source_file, destination_file)
 
         print(f"Files copied from '{source_dir}' to '{destination_dir}'.")
-
     except Exception as e:
         print(f"An error occurred: {e}")
